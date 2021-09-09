1086e8f7fc14bb0983ddc4cf0f0f8006c4a340ed
Alex Vergara <pyzyryab@tutanota.com>
2021-09-10 00:50:48 +0200

    development - Moved data related to configuration properties into a new config class.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from src.core.rumble_ai import RumbleAI
+from src.core.rumble_config import RumbleConfiguration
 
 if __name__ == '__main__':
-    rumble = RumbleAI()
+    # TODO Create the MongoDB database
+    # Get config and save config from there
+    rumble_configuration = RumbleConfiguration( 'Álex' )
+    rumble = RumbleAI( rumble_configuration )
     rumble.run()
