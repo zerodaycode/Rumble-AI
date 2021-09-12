efcbce88647f8373f8a7ff9b2c4405eae5c7605d
Alex Vergara <pyzyryab@tutanota.com>
2021-09-12 18:11:25 +0200

    MongoDB added as the Rumble persistence engine. Now it's able to retrieve itself it's basic configuration (and provide one for the first time or like the default one)

--- src/core/rumble_config.py
+++ src/core/rumble_config.py
@@ -1,24 +1,31 @@
 import pyttsx3
 
+from src.persistence import database
+
 
 class RumbleConfiguration:
     """ Stores the basic configuration properties """
-    def __init__(self, username):
-        # TODO Data should be loaded from DB
-        # Rumble dadta
-        self.assistant_name = 'Rumble'
+    def __init__(self, **configuration):
+        # Rumble data
+        self.assistant_name = configuration['assistant_name']
         # User data
-        self.username = username
+        self.username = configuration['username']
         # Voice input
-        self.mic_input_device = None
+        self.mic_input_device = configuration['mic_device']
         # Language related config
-        self.language = self.lang_setup()
-        self.id_language = 2
+        self.id_language = configuration['id_language']
+        self.language = self.speech_recognition_lang()
         # Stores the words that should be filtered to parse the user voice input
-        self.word_filter = [
-            self.assistant_name,  # ... TODO --- Complete it
-            'a', 'para', 'cabe',
-        ]
+        self.word_filter = configuration['word_filter']
+
+    def to_database(self):
+        return {
+            'assistant_name': self.assistant_name,
+            'username': self.username,
+            'mic_device': self.mic_input_device,
+            'id_language': self.id_language,
+            'word_filter': self.word_filter
+        }
 
     # TODO Change behaviour of non instance dependent methods to @staticmethods
 
@@ -31,13 +38,13 @@ class RumbleConfiguration:
 
         return tts_engine
 
-    def lang_setup(self):
+    def speech_recognition_lang(self):
         """ Returns the language code for the speech recognition API """
         sr_lang = {
             1: "en-EN",
             2: "es-ES"
         }
-        return sr_lang.get( 2, 1 )
+        return sr_lang.get( self.id_language, 1 )
 
     def mic_setup(self):
         """ Sets the correct primary audio input device """
---
+++ src/persistence/data.py
@@ -0,0 +1,9 @@
+rumble_initial_config = {
+    'assistant_name': 'Rumble',
+    'username': '',
+    'mic_device': 0,
+    'id_language': 2,
+    'word_filter': [
+        'Rumble'
+    ]
+}
